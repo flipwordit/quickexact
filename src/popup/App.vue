aaa37c739bca35b09be5391d6b3fe554f25bc1c3
A Laypanov <ostioporos@gmail.com>
2021-08-09 01:46:10 -0400

    initial popup page

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <p>Hello vue</p>
+  <n-button @click="logger">naive-ui</n-button>
 </template>
 
 <script>
+import { NButton } from 'naive-ui'
+
 export default {
-  name : 'Popup'
+  name : 'Popup',
+  components: {
+      NButton
+  },
+  methods:{
+    logger(){
+      console.log('click')
+    }
+    
+  }
 }
 </script>
 
---
+++ src/popup/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <n-button>naive-ui</n-button>
+</template>
+
+<script>
+  import { NButton } from 'naive-ui'
+
+  export default {
+    components: {
+      NButton
+    }
+  }
+</script>
