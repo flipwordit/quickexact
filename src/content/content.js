937b15e472eb9e207dfc8d7123d4eaafbcae0a73
A Laypanov <ostioporos@gmail.com>
2021-08-27 06:08:10 -0400

    include jquery

--- src/content/content.js
+++ src/content/content.js
@@ -1,1 +1,26 @@
-console.log('content from smart')
+import $ from 'jquery'
+
+if (window.location.host === 'www.google.com') {
+  const urlSearchParams = new URLSearchParams(window.location.search)
+  const params = Object.fromEntries(urlSearchParams.entries())
+
+  console.log(params.q)
+
+  $('h3', $('#search')).append('&nbsp;&#10003;')
+}
+
+let firstHref = $("a[href^='http']").eq(0).attr('href')
+
+console.log(firstHref)
+
+$("a[href^='http']").eq(0).append('*')
+
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    if (request.message === 'clicked_browser_action') {
+      let firstHref = $("a[href^='http']").eq(0).attr('href')
+
+      console.log(firstHref)
+    }
+  },
+)
