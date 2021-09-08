a46ba64f74aceddef8e882b1946e76c2f332f0a7
Mikhail Tatarkov <tmu87@mail.ru>
2021-09-08 15:48:23 +0300

    Added fuzzy search

--- popup.js
+++ popup.js
@@ -129,7 +129,11 @@ $(function () {
         return formattedDate;
       }
     },
-    { field: "title", width: "600px", cellRenderer: params => {
+    {
+      field: "title",
+      width: "600px",
+      filter: "fuzzyFilter",
+      cellRenderer: params => {
         if (!params.data)
           return params.data;
 
@@ -196,6 +200,7 @@ $(function () {
     },
     getRowNodeId: (data) => data.Id,
     components: {
+      fuzzyFilter: FuzzyFilter,
       loadingRenderer: function (params) {
         if (params.value !== undefined) {
           return params.value;
@@ -231,6 +236,12 @@ $(function () {
     refreshGrid();
   });
 
+  $('#clear-filter-btn').click(_ => clearFilters());
+
+  function clearFilters() {
+    gridOptions.api.setFilterModel(null);
+  }
+
   $('#add-block-switch').click(function () {
     $(this).html($('#add-block').toggle().is(':hidden') ? '&#8595;' : '&#8593;');
   });
@@ -265,3 +276,123 @@ chrome.runtime.onMessage.addListener(
     $(searchQueryInput).val('');
   }
 );
+
+class FuzzyFilter {
+  init(params) {
+    this.valueGetter = params.valueGetter;
+    this.filterText = null;
+    this.setupGui(params);
+
+    this.filterChangeCallBack = throttle(() => {
+      this.clearFilterValue(params);
+
+      this.#searchPrepare(params);
+
+      params.filterChangedCallback();
+    }, 1000);
+  }
+
+  clearFilterValue(params) {
+    params.api.forEachNode((node) => node.data.finded = undefined);
+  }
+  // not called by AG Grid, just for us to help setup
+  setupGui(params) {
+    this.gui = document.createElement('div');
+    this.gui.innerHTML = `<div style="padding: 4px; width: 450px;">
+                <div>
+                    <input style="margin: 4px 0 4px 0;" type="text" id="filterText" placeholder="fuzzy search..."/>
+                </div>
+                <div style="margin-top: 20px;"><table><thead><tr><th>Token</th> <th>Match type</th> <th>Description</th></tr></thead> <tbody><tr><td><code>jscript</code></td> <td>fuzzy-match</td> <td>Items that fuzzy match <code>jscript</code></td></tr> <tr><td><code>=scheme</code></td> <td>exact-match</td> <td>Items that are <code>scheme</code></td></tr> <tr><td><code>'python</code></td> <td>include-match</td> <td>Items that include <code>python</code></td></tr> <tr><td><code>!ruby</code></td> <td>inverse-exact-match</td> <td>Items that do not include <code>ruby</code></td></tr> <tr><td><code>^java</code></td> <td>prefix-exact-match</td> <td>Items that start with <code>java</code></td></tr> <tr><td><code>!^earlang</code></td> <td>inverse-prefix-exact-match</td> <td>Items that do not start with <code>earlang</code></td></tr> <tr><td><code>.js$</code></td> <td>suffix-exact-match</td> <td>Items that end with <code>.js</code></td></tr> <tr><td><code>!.go$</code></td> <td>inverse-suffix-exact-match</td> <td>Items that do not end with <code>.go</code></td></tr></tbody></table>
+                <div>
+                White space acts as an AND operator, while a single pipe (|) character acts as an OR operator.</div>
+            </div>
+        `;
+
+    const listener = (event) => {
+      this.filterText = event.target.value;
+
+      this.filterChangeCallBack();
+    };
+
+    this.eFilterText = this.gui.querySelector('#filterText');
+    this.eFilterText.addEventListener('changed', listener);
+    this.eFilterText.addEventListener('paste', listener);
+    this.eFilterText.addEventListener('input', listener);
+    // IE doesn't fire changed for special keys (eg delete, backspace), so need to
+    // listen for this further ones
+    this.eFilterText.addEventListener('keydown', listener);
+    this.eFilterText.addEventListener('keyup', listener);
+  }
+
+  getGui() {
+    return this.gui;
+  }
+
+  #searchPrepare(params) {
+    let rowData = [];
+    params.api.forEachNode(node => {
+      rowData.push(node.data);
+      node.data.finded = false;
+    });
+
+    const options = {
+      includeScore: true,
+      useExtendedSearch: true,
+      keys: ['title']
+    }
+
+    const fuse = new Fuse(rowData, options)
+
+    // Search for items that include "Man" and "Old",
+    // OR end with "Artist"
+    let searchedRows = fuse.search(this.filterText);
+
+    searchedRows.forEach(v => 
+      {
+        v.item.finded = true;
+      });
+
+    this.setFilter = true;
+  }
+
+  doesFilterPass(params) {
+    return params.data.finded === undefined || params.data.finded;
+  }
+
+  isFilterActive() {
+    return this.filterText != null && this.filterText !== '';
+  }
+
+  getModel() {
+    return { value: this.filterText.value };
+  }
+
+  setModel(model) {
+    this.eFilterText.value = model.value;
+  }
+}
+
+function throttle(func, ms) {
+
+  let savedArgs,
+    savedThis;
+
+  let timerId = null;
+  function wrapper() {
+    savedArgs = arguments;
+    savedThis = this;
+
+    if (timerId)
+      clearTimeout(timerId)
+
+    timerId = setTimeout(function () {
+      timerId = null;
+      if (savedArgs) {
+        func.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
+}
