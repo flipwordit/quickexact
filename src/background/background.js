7f6ca9da0a3f4097aa6ee694bded30306a3c9391
A Laypanov <ostioporos@gmail.com>
2021-09-08 09:23:28 -0400

    cleaned bg script

--- src/background/background.js
+++ src/background/background.js
@@ -1,23 +1,39 @@
 // for vuex sync
 // import store from '../store/index'
-console.log('background3')
-
 let color = '#3aa757'
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({ color })
-  console.log('Default background color set to %cgreen', `color: ${color}`)
-})
-async function move(activeInfo) {
-  console.log(activeInfo)
-}
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ color });
+//   console.log('Default background color set to %cgreen', `color: ${color}`);
+// });
 
-chrome.tabs.onActivated.addListener(activeInfo => move(activeInfo))
+// chrome.tabs.onActivated.addListener(activeInfo => move(activeInfo));
 
-chrome.commands.onCommand.addListener((command) => {
+// async function move(activeInfo) {
+//   console.log(activeInfo);
+// }
+
+chrome.commands.onCommand.addListener(async (command) => {
   console.log('Command:' + command)
+
+  // switch (command) {
+  //   case "open-search": {
+  //     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  //     //Current tab is this extension
+  //     //TODO: relace to find if current extension open in tabs and make it active
+  //     //Do we need many tabs with smartoteka? Many windows?
+  //     if (tab.url === "chrome-extension://fkfammijpebbgdjblnmkkmobgenppkda/popup.html") {
+  //       chrome.tabs.sendMessage(tab.id, "clear", function(response) {
+  //         console.log(response);
+  //       });
+  //     } else {
+  chrome.tabs.create({ url: 'popup.html' })
+  //     }
+  //   }
+  // }
 })
 
-chrome.action.onClicked.addListener((tab) => {
-  console.log('Tab:' + tab)
-})
+// chrome.action.onClicked.addListener((tab) => {
+//   console.log("Tab:" + tab);
+// });
