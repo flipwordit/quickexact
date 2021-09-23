70486bc722feeac5c8a974cf7c85c12d94507bd6
A Laypanov <ostioporos@gmail.com>
2021-09-23 08:59:02 -0400

    include feather icons

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,9 +1,18 @@
+// eslint-disable-next-line import/no-unresolved
+import 'windi.css'
 import './popup.scss'
+import 'element-plus/dist/index.css'
 
 import { createApp } from 'vue'
 
-import App from './App.vue'
+import ElementPlus from 'element-plus'
+import VueFeather from 'vue-feather'
+
+import App from './App'
 
 const app = createApp(App)
+
+app.use(ElementPlus)
+app.component(VueFeather.name, VueFeather)
 
 app.mount('#app')
