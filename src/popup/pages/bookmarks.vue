53e51d47b0039d15c0adbef6e2942a5aa8df9277
A Laypanov <ostioporos@gmail.com>
2021-10-14 03:21:27 -0400

    bookmarks

--- src/popup/pages/bookmarks.vue
+++ src/popup/pages/bookmarks.vue
@@ -1,13 +1,30 @@
 <template>
-  <h1>Bookamarks</h1>
+<!-- <pre>{{bookmarks}}</pre> -->
+<div class="page">
+   <Tree v-for="bookmark in bookmarks" :key="bookmark.id" :treeData="bookmark" />
+</div>
+
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import Tree from '@/popup/components/Tree'
+
 export default {
   name: 'Bookmarks',
+  components: {
+    Tree,
+  },
+  computed: {
+    ...mapGetters({
+      bookmarks: 'getBookmarks',
+    }),
+  },
 }
 </script>
 
 <style>
-
+.page {
+  overflow: scroll;
+}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,47 +1,86 @@
 import { createStore } from 'vuex'
-import Fuse from 'fuse.js'
 import { debounce } from 'lodash'
 import tabsService from '@/services/tabs'
+import historyService from '@/services/history'
+import bookmarksService from '@/services/boodmarks'
 
 const store = createStore({
   state() {
     return {
-      tabs: [],
+      windows: [],
       sessions: [],
       bookmarks: [],
-      history: [],
+      records: [],
     }
   },
   getters: {
     getQuery(state) {
       return state.query
     },
+    tabsCount(state) {
+      return state.windows.reduce((acc, item) => {
+        acc += item.tabs.length
+        return acc
+      }, 0)
+    },
+    recordsCount(state) {
+      return state.records.length
+    },
+    getBookmarks: (state) => state.bookmarks,
+    getRecords: (state) => state.records,
     getWindows: (state) => state.windows,
   },
   mutations: {
     UPDATE_WINDOWS(state, payload) {
       state.windows = payload
     },
+    UPDATE_RECORDS(state, payload) {
+      state.records = payload
+    },
+    UPDATE_BOOKMARKS(state, payload) {
+      state.bookmarks = payload
+    },
   },
   actions: {
     async collectWindows({ commit }, query) {
       const allWindows = await tabsService.getWindows()
-      const options = {
-        includeScore: true,
-        keys: ['tabs.title', 'tabs.url'],
-      }
 
       if (query) {
-        const fuse = new Fuse(allWindows, options)
-        const windows = fuse.search(query)
+        const windows = allWindows.filter(item => {
+          item.tabs.filter(tab => tab.title.includes('-'))
+          return Boolean(item)
+        })
+        console.log(windows.length)
+        // console.log(windows)
         commit('UPDATE_WINDOWS', windows)
       } else {
         commit('UPDATE_WINDOWS', allWindows)
       }
+
+      // const options = {
+      //   includeScore: true,
+      //   keys: ['tabs.title', 'tabs.url'],
+      // }
+
+      // if (query) {
+      //   const fuse = new Fuse(allWindows, options)
+      //   const windows = fuse.search(query)
+      //
+      // } else {
+      //   commit('UPDATE_WINDOWS', allWindows)
+      // }
     },
     collectSessions() {},
-    collectHistory() {},
-    collectBookmarks() {},
+    async collectHistory({ commit }, query = '') {
+      console.log(query)
+      const records = await historyService.search(query)
+      commit('UPDATE_RECORDS', records)
+    },
+    async collectBookmarks() {
+      const bookmarks = await bookmarksService.getTree()
+      console.log(bookmarks)
+      this.commit('UPDATE_BOOKMARKS', bookmarks)
+    },
   },
 })
 
